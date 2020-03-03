40eac8ee6c0baf642ecadcc4f3389cb64ffffa67
jisu <282532@likelion.org>
2020-03-03 13:20:16 +0900

    err

--- xml_to_excel/api_crawler_all.py
+++ xml_to_excel/api_crawler_all.py
@@ -27,7 +27,7 @@
 #http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd
 
 #유진증권
-#http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId= 15&offset=1221195&length=219209&dtd=dart3.xsd
+#http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd
 
 
 import time
@@ -35,29 +35,84 @@ from bs4 import BeautifulSoup
 import urllib.parse as parser
 import selenium
 import pandas as pd
+import os
 from html_table_parser import parser_functions as parser
 from openpyxl import load_workbook
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import traceback
 
 
-url2="http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd"
+def url_to_excel(com_url):
+    api_key = "fbd3f31ee413a318c81b0fe2bc0ad8b283dcfe21"
 
-report=urlopen(url2)
-r=report.read()
+    if com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd":
+        report= urlopen(com_url)
+        r=report.read()
 
-xmlsoup=BeautifulSoup(r,'html.parser')
-body = xmlsoup.find("body")
-table = body.find_all("table")
-p = parser.make2d(table[1])
+        xmlsoup=BeautifulSoup(r,'html.parser')
+        body = xmlsoup.find("body")
+        table = body.find_all("table")
+        p = parser.make2d(table[1])
 
-df = pd.DataFrame(p[0:])
-header = df.iloc[0]
+        df = pd.DataFrame(p[0:])
+        header = df.iloc[0]
+        df.rename (columns = header , inplace = True)
+        df = df.reset_index(inplace=True)
 
-df.rename (columns = header , inplace = True)
-df=df.set_index('')
+    else:
+        try:
+            report=urlopen(com_url)
+            r=report.read()
 
-writer = pd.ExcelWriter('ex_test.xlsx', engine = 'xlsxwriter')
-df.to_excel(writer, sheet_name = '유진기업')
-writer.save()
+            xmlsoup=BeautifulSoup(r,'html.parser')
+            body = xmlsoup.find("body")
+            table = body.find_all("table")
+            p = parser.make2d(table[1])
+
+            df = pd.DataFrame(p[0:])
+            header = df.iloc[0]
+
+            df.rename (columns = header , inplace = True)
+            df=df.set_index('')
+        
+        except Exception as e :
+            print(traceback.format_exc())
+
+
+    if not os.path.exists('output.xlsx'):#파일 초기에 생성하기 위해 유진기업은 mode = "w"로 지정!
+        with pd.ExcelWriter('output.xlsx', mode='w', engine='openpyxl') as writer:
+            if com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd":
+                df.to_excel(writer, sheet_name = '유진기업', startrow = 1, startcol = 1) 
+                writer.save()
+                writer.close()
+
+            else :
+                print("url이 잘못되었습니다.")
+                
+    else:#만약 이미 파일이 존재한다면 그 파일에 시트를 append! 
+        with pd.ExcelWriter('output.xlsx', mode='a', engine='openpyxl') as writer:
+            if com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd":
+                df.to_excel(writer, sheet_name = '동양', startrow = 1, startcol = 1)
+                writer.save()
+                writer.close()
+            
+            elif com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd":
+                df.to_excel(writer, sheet_name = '유진증권', startrow = 1, startcol = 1)
+                writer.save()
+                writer.close()
+
+            else :
+                print("잘못입력하셨습니다.")
+    return 
+
+
+
+#실행 코드 
+url_list = ["http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd", #유진기업
+            "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd", #동양
+            "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd"] #유진증권
+
+for com_url in url_list:
+    url_to_excel(com_url)
