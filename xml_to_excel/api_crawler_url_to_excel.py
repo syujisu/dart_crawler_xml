12af785f8010523357fcc59fad11c70ab32cf7c7
jisu <282532@likelion.org>
2020-03-04 14:14:58 +0900

    second

--- xml_to_excel/api_crawler_url_to_excel.py
+++ xml_to_excel/api_crawler_url_to_excel.py
@@ -1,4 +1,4 @@
-#2018년 사업보고서를 찾아서 엑셀을 완성하십시오.
+# 2018년 사업보고서를 찾아서 엑셀을 완성하십시오.
 # 2. 크롤링
 # https://dart.fss.or.kr/dsaf001/main.do?rcpNo=(예:유진증권 20190401002982)
 # 를 하면 좌측 메뉴에 III.재무에 관한사항 - 4.재무제표를 선택하면
@@ -7,14 +7,14 @@
 # - 포괄상태표"이하는 무시하십시오.
 # - 유진기업, 동양, 유진증권 3개 기업을 대상으로 하십시오
 
-#유진기업 2018 재무제표 url
-#http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd
+# 유진기업 2018 재무제표 url
+# http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd
 
-#동양
-#http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd
+# 동양
+# http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd
 
-#유진증권
-#http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd
+# 유진증권
+# http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd
 
 
 import time
@@ -30,27 +30,29 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import traceback
 
+
 def url_to_excel(com_url):
     api_key = "fbd3f31ee413a318c81b0fe2bc0ad8b283dcfe21"
 
     if com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd":
-        report= urlopen(com_url)
-        r=report.read()
+        report = urlopen(com_url)
+        r = report.read()
 
-        xmlsoup=BeautifulSoup(r,'html.parser')
+        xmlsoup = BeautifulSoup(r, 'html.parser')
         body = xmlsoup.find("body")
         table = body.find_all("table")
         p = parser.make2d(table[1])
 
-        df = pd.DataFrame(p[0:], columns=['과목','2018.12.31','2018.12.31','2017.12.31','2017.12.31','2016.12.31','2016.12.31'])
+        df = pd.DataFrame(p[0:], columns=['과목', '2018.12.31', '2018.12.31',
+                                          '2017.12.31', '2017.12.31', '2016.12.31', '2016.12.31'])
         df = df.set_index('과목')
 
     else:
         try:
-            report=urlopen(com_url)
-            r=report.read()
+            report = urlopen(com_url)
+            r = report.read()
 
-            xmlsoup=BeautifulSoup(r,'html.parser')
+            xmlsoup = BeautifulSoup(r, 'html.parser')
             body = xmlsoup.find("body")
             table = body.find_all("table")
             p = parser.make2d(table[1])
@@ -58,43 +60,43 @@ def url_to_excel(com_url):
             df = pd.DataFrame(p[0:])
             header = df.iloc[0]
 
-            df.rename (columns = header , inplace = True)
-            df=df.set_index('')
-        
-        except Exception as e :
+            df.rename(columns=header, inplace=True)
+            df = df.set_index('')
+
+        except Exception as e:
             print(traceback.format_exc())
 
-
-    if not os.path.exists('output.xlsx'):#파일 초기에 생성하기 위해 유진기업은 mode = "w"로 지정!
+    if not os.path.exists('output.xlsx'):  # 파일 초기에 생성하기 위해 유진기업은 mode = "w"로 지정!
         with pd.ExcelWriter('output.xlsx', mode='w', engine='openpyxl') as writer:
             if com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd":
-                df.to_excel(writer, sheet_name = '유진기업', startrow = 1, startcol = 1) 
+                df.to_excel(writer, sheet_name='유진기업', startrow=1, startcol=1)
                 writer.save()
                 writer.close()
 
-            else :
+            else:
                 print("url이 잘못되었습니다.")
-                
-    else:#만약 이미 파일이 존재한다면 그 파일에 시트를 append! 
+
+    else:  # 만약 이미 파일이 존재한다면 그 파일에 시트를 append!
         with pd.ExcelWriter('output.xlsx', mode='a', engine='openpyxl') as writer:
             if com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd":
-                df.to_excel(writer, sheet_name = '동양', startrow = 1, startcol = 1)
+                df.to_excel(writer, sheet_name='동양', startrow=1, startcol=1)
                 writer.save()
                 writer.close()
-            
+
             elif com_url == "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd":
-                df.to_excel(writer, sheet_name = '유진증권', startrow = 1, startcol = 1)
+                df.to_excel(writer, sheet_name='유진증권', startrow=1, startcol=1)
                 writer.save()
                 writer.close()
 
-            else :
+            else:
                 pass
-    return 
+    return
 
-#실행 코드 
-url_list = ["http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd", #유진기업
-            "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd", #동양
-            "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd"] #유진증권
+
+# 실행 코드
+url_list = ["http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401004107&dcmNo=6612848&eleId=15&offset=1908112&length=146218&dtd=dart3.xsd",  # 유진기업
+            "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190814001889&dcmNo=6845532&eleId=17&offset=1354185&length=99327&dtd=dart3.xsd",  # 동양
+            "http://dart.fss.or.kr/report/viewer.do?rcpNo=20190401002982&dcmNo=6607495&eleId=15&offset=1221195&length=219209&dtd=dart3.xsd"]  # 유진증권
 
 for com_url in url_list:
     url_to_excel(com_url)
--- xml_to_excel/api_crawler_xml_to_excel.py
+++ xml_to_excel/api_crawler_xml_to_excel.py
@@ -1,4 +1,4 @@
- # 1. xml 다운로드
+# 1. xml 다운로드
 # 공시정보-공시보고서원본파일
 # https://opendart.fss.or.kr/api/document.xml
 # ?crtfc_key=*****&rcept_no=(예:유진증권 20190401002982)
@@ -13,8 +13,12 @@
 
 # 20190401004107 유진
 # https://opendart.fss.or.kr/api/document.xml?crtfc_key=fbd3f31ee413a318c81b0fe2bc0ad8b283dcfe21&rcept_no=20190401004107
+
 # 20190814001889 동양
+# https://opendart.fss.or.kr/api/document.xml?crtfc_key=fbd3f31ee413a318c81b0fe2bc0ad8b283dcfe21&rcept_no=20190401003691
+
 # 20190401002982 유진증권
+# https://opendart.fss.or.kr/api/document.xml?crtfc_key=fbd3f31ee413a318c81b0fe2bc0ad8b283dcfe21&rcept_no=20190401002982
 import time
 from bs4 import BeautifulSoup
 import urllib.parse as parser
@@ -26,22 +30,62 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import traceback
 import os
+import zipfile
+
+# 00184667 유진기업
+# 00131054 유진증권
+# 00117337 동양
+# 00163266 한일합섬 *
+# 00165149 유진저축은행
+# rcept_no 찾기
+
+company_code_list = ['00184667', '00117337', '00131054']
 
 
+# report_no 찾기 : xml -> rcept_no
+def report_no():
+    API_KEY = "fbd3f31ee413a318c81b0fe2bc0ad8b283dcfe21"
 
-fp=open(r'C:\Users\user\Desktop\document\1.xml',"r")
-soup=BeautifulSoup(fp,'html.parser', encoding = 'utf-8')
-body = soup.find('body')
-table = body.findAll('table')
+    url = "https://opendart.fss.or.kr/api/list.xml?crtfc_key="+API_KEY+"&corp_code="+company_code + \
+        "&bgn_de=20160101&end_de=20191231&pblntf_ty=A&pblntf_detail_ty=A002&page_no=1&page_count=10"
+
+    resultXML = urlopen(url)
+    result = resultXML.read()
+    xmlsoup = BeautifulSoup(result, 'html.parser')
+    data = pd.DataFrame()
+    te = xmlsoup.findAll("list")
+
+    for t in te:
+        temp = pd.DataFrame(([[t.corp_cls.string, t.corp_name.string, t.corp_code.string, t.stock_code.string,
+                               t.report_nm.string, t.rcept_no.string, t.flr_nm.string, t.rcept_dt.string, t.rm.string]]),
+                            columns=["corp_cls", "corp_name", "corp_code", "stock_code", "report_nm", "rcept_no", "flr_nm", "rcept_dt", "rm"])
+        data = pd.concat([data, temp])
+
+    data = data.reset_index(drop=True)
+
+    report_code = data[data['report_nm'] == '사업보고서 (2018.12)']
+    rcept_no = report_code['rcept_no']
+
+    return rcept_no
 
 
-for i in range(len(table)):
-    a=pd.DataFrame(parser.make2d(table[i]))
-    
-    if a.iloc[0,0]=='재무상태표':
-        df=pd.DataFrame(parser.make2d(table[i+1]))
-        break
+for company_code in company_code_list:
+    report_no()
 
-# fp = open(r"C:\Users\user\Desktop\document\1.xml", "r")
 
-# soup = BeautifulSoup(fp, 'html.parser')
+# fp = open(r'C:\Users\user\Desktop\document\1.xml', "r")
+# soup = BeautifulSoup(fp, 'html.parser', encoding='utf-8')
+# body = soup.find('body')
+# table = body.findAll('table')
+
+
+# for i in range(len(table)):
+#     a = pd.DataFrame(parser.make2d(table[i]))
+
+#     if a.iloc[0, 0] == '재무상태표':
+#         df = pd.DataFrame(parser.make2d(table[i+1]))
+#         break
+
+# # fp = open(r"C:\Users\user\Desktop\document\1.xml", "r")
+
+# # soup = BeautifulSoup(fp, 'html.parser')
